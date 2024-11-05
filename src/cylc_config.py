c26f8af06c326ed41edf1582f1a7e545c97614c6
Antonio Mariani <antonio.mariani@cmcc.it>
2024-11-05 13:25:43 +0100

    remove not used func

--- src/cylc_config.py
+++ src/cylc_config.py
@@ -7,10 +7,6 @@ DEFAULT_EXTEND = False
 CYLC_SRC_FLOW_NAME = "flow.cylc"
 
 
-def get_conda_env() -> str:
-    return os.environ["CYLC_CONDA_ENV"]
-
-
 def cylc_run() -> Path:
     """Cylc run path"""
     return Path(os.environ["CYLC_RUN_DIR"])
